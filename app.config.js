c54a747ee0ae27c8535a93f63e96375c2c965263
Xavier Comi <xaviercomi@gmail.com>
2025-04-15 16:34:50 +0200

    app.config,..env, eliminar app.json, nuevo API KEY

---
+++ app.config.js
@@ -0,0 +1,38 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "Sant Jordi toolbox",
+    slug: "sant-jordi-toolbox",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.xaviercomi.SantJordiToolbox",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+      API_URL: process.env.API_URL,
+      eas: {
+        projectId: "7228a073-eb5c-4550-b0c3-5918b829393d",
+      },
+    },
+  },
+};
